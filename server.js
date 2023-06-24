e2b6cf6c4c1e326fe4708287e9a1aa07cc8f044b
raymond88wang <raymond88wang@gmail.com>
2023-06-24 11:06:19 -0700

    print mongodb uri

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express()
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
   () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
+console.log(process.env.MONGO_URI)
 
 // MIDDLEWARE
 app.use(methodOverride('_method'))
